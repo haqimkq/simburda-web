c313b49e6186177adde1a61a0361f7936d89b71d
rizki2853 <72205902+rizki2853@users.noreply.github.com>
2023-07-11 11:16:54 +0800

    update migration

--- database/migrations/2023_07_11_025046_create_pengembalian_penggunaan_details_table.php
+++ database/migrations/2023_07_11_025046_create_pengembalian_penggunaan_details_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -33,6 +34,8 @@ return new class extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::dropIfExists('pengembalian_penggunaan_details');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 };
