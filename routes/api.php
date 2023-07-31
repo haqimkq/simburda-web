fb5b02d3aa3863eacc064d113ba062b6338d2ca6
Pangeran Muhammad Waliyullah <pangeranwaliyullah@gmail.com>
2023-07-31 12:40:05 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Api\ProvinceController;
 use App\Http\Controllers\Api\ProyekController;
 use App\Http\Controllers\Api\SuratJalanController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\Api\UserController;
 use App\Models\Proyek;
 
 /*
